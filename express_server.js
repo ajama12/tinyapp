d8ee23309c9c75294d58a5c4b77fc71262b576e9
Asma Jama <asmajama98@gmail.com>
2023-04-10 18:07:38 -0400

    Implement logout feature

--- express_server.js
+++ express_server.js
@@ -73,9 +73,13 @@ app.post("/urls/:id/delete", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-  const username = req.body.username;
-  res.cookie("username", username);
+  res.cookie('username', req.body.username);
   res.redirect("urls");
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls");
 });
 
 app.listen(PORT, () => {
