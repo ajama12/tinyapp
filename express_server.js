7fc036532b4e5d790f52ecbbb0c43ba85fa253eb
Asma Jama <asmajama98@gmail.com>
2023-04-10 16:07:09 -0400

    Add route for /urls and add basic HTML template

--- express_server.js
+++ express_server.js
@@ -1,7 +1,8 @@
-app.set("view engine", "ejs")
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+
+app.set("view engine", "ejs");
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
@@ -22,4 +23,12 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls/:id", (req, res) => {
+  const templateVars = {
+    id: req.params.id,
+    longURL: urlDatabase[req.params.shortURL],
+  };
+  res.render("urls_show", templateVars);
 });
