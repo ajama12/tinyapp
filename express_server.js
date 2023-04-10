72ed3bb1d7d174fd2af4407fc30b0456cb8ebdcb
Asma Jama <asmajama98@gmail.com>
2023-04-10 16:56:07 -0400

    Fix string generator, add redirects, fix formatting

--- express_server.js
+++ express_server.js
@@ -4,21 +4,17 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
-function generateRandomString() {
-  return Math.random().toString(36).substring(2, 6)
-}
-
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
 
-app.use(express.urlencoded({ extended: true }));
+//generate a random 6-character string
+function generateRandomString() {
+  return Math.random().toString(36).substring(6);
+}
 
-app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
-});
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -36,6 +32,11 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = {
     id: req.params.id,
@@ -44,9 +45,9 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+app.get("/u/:id", (req, res) => {
+  const longUrl = urlDatabase[req.params.id];
+  res.redirect(longUrl);
 });
 
 app.listen(PORT, () => {
